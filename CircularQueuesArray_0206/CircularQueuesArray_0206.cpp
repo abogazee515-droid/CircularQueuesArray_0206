35e6a017defdf5d40b164cf1d5c8fb09aa4ceef6
abogazee515-droid <abogazee515@gmail.com>
2026-05-19 14:19:08 +0700

    Add overflow condition to prevent inserting elements when the queue is full

--- CircularQueuesArray_0206/CircularQueuesArray_0206.cpp
+++ CircularQueuesArray_0206/CircularQueuesArray_0206.cpp
@@ -24,6 +24,12 @@ public:
         cin >> num;
         cout << endl;
 
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n";
+            return;
+        }
+
     }
 
 };
